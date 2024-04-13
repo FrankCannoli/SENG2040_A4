e690c3d8f474833a04eb09e98e953596236d3727
FrankCannoli <107089433+FrankCannoli@users.noreply.github.com>
2024-04-13 18:19:14 -0400

    Added a form to the modal, included alerts for post success or failure

--- posts/forms.py
+++ posts/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Post
 
 class PostForm(forms.ModelForm):
+    # FOLLOWING LINES ARE USED IF IMPLEMENTING FORM WITHOUT CRISPY FORMS
     # title = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     # body = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 3}))
     class Meta:
