dd6d4963bbdfad8ac23b73aaef79f4df2fffd40c
FrankCannoli <107089433+FrankCannoli@users.noreply.github.com>
2024-04-15 00:23:16 -0400

    added limitations to dropzone

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -16,7 +16,9 @@ const url = window.location.href
 
 const alertBox = document.getElementById('alert-box')
 
-console.log('csrf', csrf[0].value)
+const dropzone = document.getElementById('my-dropzone')
+const addBtn = document.getElementById('add-btn')
+const closeBtns = [...document.getElementsByClassName('add-modal-close')]
 
 
 const getCookie =(name) => {
@@ -93,7 +95,7 @@ const getData = () => {
                                     </div>
                                     <div class="col-2">
                                         <form class="like-unlike-forms" data-form-id="${el.id}">
-                                            <button href="#" class="btn btn-primary" id="like-unlike-${el.id}">${el.liked ? `Unlike (${el.count})`: `Like (${el.count})`}</button>
+                                            <button class="btn btn-primary" id="like-unlike-${el.id}">${el.liked ? `Unlike (${el.count})`: `Like (${el.count})`}</button>
                                         </form>
                                     </div>
                                 </div>
@@ -126,6 +128,8 @@ loadBtn.addEventListener('click', ()=>{
     getData()
 })
 
+
+let newPostId = null
 postForm.addEventListener('submit', e=>{
     e.preventDefault()
 
@@ -139,36 +143,62 @@ postForm.addEventListener('submit', e=>{
         },
         success: function(response) {
             console.log(response)
+            newPostId = response.id
             postsBox.insertAdjacentHTML('afterbegin', `
-            <div class="card mb-2">
-                <div class="card-body">
-                    <h5 class="card-title">${response.title}</h5>
-                    <p class="card-text">${response.body}</p>
-                </div>
-                <div class="card-footer">
-                    <div class="row">
-                        <div class="col-2">
-                            <a href="#" class="btn btn-primary">Details</a>
-                        </div>
-                        <div class="col-2">
-                            <form class="like-unlike-forms" data-form-id="${response.id}">
-                                <button href="#" class="btn btn-primary" id="like-unlike-${response.id}">Like (0)</button>
-                            </form>
+                <div class="card mb-2">
+                    <div class="card-body">
+                        <h5 class="card-title">${response.title}</h5>
+                        <p class="card-text">${response.body}</p>
+                    </div>
+                    <div class="card-footer">
+                        <div class="row">
+                            <div class="col-2">
+                                <a href="${url}${response.id}" class="btn btn-primary">Details</a>
+                            </div>
+                            <div class="col-2">
+                                <form class="like-unlike-forms" data-form-id="${response.id}">
+                                    <button class="btn btn-primary" id="like-unlike-${response.id}">Like (0)</button>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
             `)
             likeUnlikePosts()
-            $('#addPostModal').modal('hide')
+            // $('#addPostModal').modal('hide')
             handleAlerts('success', 'New post added!')
-            postForm.reset()
+            // postForm.reset()
         },
         error: function(error) {
             console.log(error)
             handleAlerts('danger', 'oops...something went wrong')
         }
     })
+})
+
+addBtn.addEventListener('click', ()=>{
+    dropzone.classList.remove('not-visible')
+})
+
+closeBtns.forEach(btn=> btn.addEventListener('click', ()=>{
+    postForm.reset()
+    if (!dropzone.classList.contains('not-visible')) {
+        dropzone.classList.add('not-visible')
+    }
+}))
+
+Dropzone.autoDiscover = false
+const myDropzone = new Dropzone('#my-dropzone', {
+    url: 'upload/',
+    init: function() {
+        this.on('sending', function(file, xhr, formData){
+            formData.append('csrfmiddlewaretoken', csrftoken)
+            formData.append('new_post_id', newPostId)
+        })
+    },
+    maxFiles: 5,
+    maxFilesize: 4,
+    acceptedFiles: '.png, .jpg, .jpeg'
 })
 
 
